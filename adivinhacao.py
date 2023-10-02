55933336971fada2e05911fee80857cd2f34a09a
Murillo <srg.mrll@gmail.com>
2023-10-02 20:57:39 -0300

    Jogo de adivinhação. Exercício curso Alura

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,12 +5,15 @@ print("**********************************")
 print("Bem vindo ao jogo da Adivinhação!")
 print("**********************************")
 
-chute = int(input("Informe um número entre 1 e 20: "))
-
-if (chute == num_random):
-    print("Acertou!")
-else:
-    if (chute > num_random):
-        print("Chutou um número maior!")
+passe = 1
+while(passe==1):
+    chute = int(input("Informe um número entre 1 e 20: "))
+    if (chute == num_random):
+        print("Acertou!")
+        break
     else:
-        print("Chutou um número menor!")
+        if (chute > num_random):
+            print("Chutou um número maior!")
+        else:
+            print("Chutou um número menor!")
+    passe = int(input("Informe 0 para deixar o programa e 1 para tentar novamente!\n"))
