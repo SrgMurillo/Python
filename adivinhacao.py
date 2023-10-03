ac68ec1f0c234a1f21cf6e5c09e97c7cdd0f12f8
Murillo <srg.mrll@gmail.com>
2023-10-03 10:05:15 -0300

    Incluido níveis e pontuação

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,23 +1,33 @@
 import random
 
-num_random = random.randint(1, 20)
+num_random = random.randint(1, 10)
 print("**********************************")
 print("Bem vindo ao jogo da Adivinhação!")
 print("**********************************")
 
-tentativas = 1
-while(tentativas <= 3):
+print("Qual nível de dificuldade?")
+print("Selecione (1)Difícil (2)Médio (3)Fácil")
+
+nivel = int(input("Definal o nivel: "))
+
+if(nivel == 1):
+    x = 10
+elif nivel == 2:
+    x = 15
+else:
+    x = 20
+
+for tentativas in range(x):
     print("Tentativa número ", tentativas)
-    chute = int(input("Informe um número entre 1 e 20: "))
+    chute = int(input("Informe um número entre 1 e 100: "))
     if (chute == num_random):
-        print("Acertou!")
+        print("Acertou! Você ficou com {} pontos".format(pontos_perdidos))
         break
     else:
         if (chute > num_random):
             print("Chutou um número maior!")
         else:
             print("Chutou um número menor!")
-
-    tentativas += 1
+    pontos_perdidos = num_random - tentativas
 
 print("*******************  Fim do Jogo  *******************")
