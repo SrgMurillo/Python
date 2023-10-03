9d1c7bc4963666500d8fed8918b98e063aec30f0
Murillo <srg.mrll@gmail.com>
2023-10-02 21:17:31 -0300

    Alteração no loop de tentativas

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,8 +5,9 @@ print("**********************************")
 print("Bem vindo ao jogo da Adivinhação!")
 print("**********************************")
 
-passe = 1
-while(passe==1):
+tentativas = 1
+while(tentativas <= 3):
+    print("Tentativa número ", tentativas)
     chute = int(input("Informe um número entre 1 e 20: "))
     if (chute == num_random):
         print("Acertou!")
@@ -16,4 +17,7 @@ while(passe==1):
             print("Chutou um número maior!")
         else:
             print("Chutou um número menor!")
-    passe = int(input("Informe 0 para deixar o programa e 1 para tentar novamente! \n"))
+
+    tentativas += 1
+
+print("*******************  Fim do Jogo  *******************")
